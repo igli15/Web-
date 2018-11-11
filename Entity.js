b2ddf740a4a6b20d5ad07737e7624bd66e56d0f2
Igli milaqi <milaqiigli@gmail.com>
2018-11-11 01:36:49 +0100

    cleaned up the entity class.

--- Entity.js
+++ Entity.js
@@ -4,10 +4,9 @@ class Entity
 	constructor()
 	{
 		this.manager = Canvas.getEntityManager.addEntity(this);
-		
+	
 		this.x = 0;
 		this.y = 0;
-		this.r = 100;
 		Entity.count += 1;
 		this.id = Entity.count;
 		
@@ -20,12 +19,7 @@ class Entity
 
 	update()
 	{
-		this.render();
-	}
-
-	render()
-	{
-		ellipse(this.x, this.y, this.r*2, this.r*2);
+		
 	}
 
 	setPos(px,py)
